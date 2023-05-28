1ad9bfcdccbc53c076f490493d16a5a8cfcaacb3
NGÂN NGUYỄN THỊ KIỀU <21120506@student.hcmus.edu.vn>
2023-05-28 23:16:43 +0700

    Added my project

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const app = express()
+const ejs = require('ejs');
+const fs = require('fs');
+const port = 3000;
+var path = require('path')
+
+//database connection
+
+
+//static file
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+// Render index.ejs file
+app.get('/', function (req, res) {
+ 
+    // Render page using renderFile method
+    ejs.renderFile('view/home/index.ejs', {},
+        {}, function (err, template) {
+            if (err) {
+                throw err;
+            } else {
+                res.end(template);
+            }
+        });
+});
+ 
+// Server setup
+app.listen(port, function (error) {
+    if (error)
+        throw error;
+    else
+        console.log("Server is running");
+});
